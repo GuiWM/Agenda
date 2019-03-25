cdd9db3dd28a7dc188f707b6a47922b164d8ca5f
Guilherme <gwmagnabosco@hotmail.com>
2019-03-25 18:32:54 -0300

    Primeira versão

---
+++ agenda4semestre.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void *pbuffer;
+
+typedef struct var{
+	int escolha, contador, i;
+} variaveis;
+
+typedef struct pe {
+   char nome[25];
+} pessoa;
+
+variaveis *inicio;
+
+void inserir();
+void listar();
+
+int main () {
+
+    printf("---------- AGENDA ------------");
+
+    pbuffer = malloc(sizeof(variaveis));
+
+    inicio = pbuffer;
+
+    inicio->contador = 0;
+
+    do {
+        printf("\n[>1<] INSERIR\n[>2<] LISTAR\n[>5<] SAIR\n");
+        scanf("%d", &inicio->escolha);
+
+		switch (inicio->escolha) {
+			case 1:
+				inserir();
+				break;
+			case 2:
+				listar();
+				break;
+			default:
+                free(pbuffer);
+                exit(1);
+                break;
+		}
+	} while (1);
+
+}
+void inserir() {
+
+    pessoa *p;
+
+    if ((pbuffer = realloc(pbuffer, sizeof(variaveis) + sizeof(pessoa) + (sizeof(pessoa) * inicio->contador))) == NULL) {
+       printf("Falta de memória!");
+       exit(1);
+    }
+
+    p = pbuffer + sizeof(variaveis) + (sizeof(pessoa) * inicio->contador);
+
+    printf("\nNome: ");
+    scanf("%s", p->nome);
+
+
+    inicio->contador++;
+}
+void listar() {
+
+   pessoa *p;
+
+   p = pbuffer + sizeof(variaveis);
+
+   for (inicio->i = 0; inicio->i < inicio->contador; inicio->i++) {
+        printf("\n%s", p->nome);
+        p++;
+   }
+
+}
