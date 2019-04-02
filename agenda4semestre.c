ac65b55f183ab8858cd57e4ce815698be1ddad92
Guilherme <gwmagnabosco@hotmail.com>
2019-04-02 14:15:45 -0300

    Adicionado buscar e alterando apagar

--- agenda4semestre.c
+++ agenda4semestre.c
@@ -5,11 +5,12 @@
 void *pbuffer;
 
 typedef struct var{
-	int escolha, contador, i;
+	int escolha, contador, i, achei, posicao;
+	char nome[20];
 } variaveis;
 
 typedef struct pe {
-   char nome[25], cpf[15];
+   char nome[20], cpf[15];
 } pessoa;
 
 variaveis *inicio;
@@ -17,6 +18,7 @@ variaveis *inicio;
 void inserir();
 void listar();
 void apagar();
+void buscar();
 
 int main () {
 
@@ -29,7 +31,7 @@ int main () {
     inicio->contador = 0;
 
     do {
-        printf("\n[>1<] INSERIR\n[>2<] LISTAR\n[>3<] APAGAR\n[>5<] SAIR\n");
+        printf("\n[>1<] INSERIR\n[>2<] LISTAR\n[>3<] APAGAR\n[>4<] BUSCAR\n[>5<] SAIR\n");
         scanf("%d", &inicio->escolha);
 
 		switch (inicio->escolha) {
@@ -41,6 +43,9 @@ int main () {
 				break;
 			case 3:
 				apagar();
+				break;	
+			case 4:
+				buscar();
 				break;	
 			default:
                 free(pbuffer);
@@ -84,7 +89,7 @@ void listar() {
 
 }
 void apagar() {
-	
+	/*
 	if (inicio->contador == 0) {
 		printf("------------");
 		printf("AGENDA VAZIA!");
@@ -92,6 +97,79 @@ void apagar() {
 	} else if ((pbuffer = (pessoa *) realloc(pbuffer, sizeof(variaveis) + (sizeof(pessoa) * inicio->contador))) != NULL) {
 		printf("\n-------------------------\nPessoa excluida com sucesso...\n-------------------------\n");	
 		inicio->contador--;
+	}	*/
+	
+	inicio->achei = 0;
+	inicio->posicao = 0;
+	
+	printf("Nome a ser apagado: ");
+	scanf("%s", inicio->nome);
+	
+	pessoa *p;
+	
+	p = pbuffer + sizeof(variaveis);
+	
+	for (inicio->i = 0; inicio->i < inicio->contador; inicio->i++) {
+		if (strcmp(p->nome, inicio->nome) == 0) {
+			inicio->achei = 1;	
+			inicio->posicao = inicio->i;
+		}	
+		p++;
 	}	
+	
+	if (inicio->achei == 0) {
+		printf("\n--------------------------\n");
+		printf("  Nome nao encontrado...\n");
+		printf("--------------------------\n");	
+	} else {
+		
+		pessoa *aux;
+		
+		p = pbuffer + sizeof(variaveis) + (sizeof(pessoa) * inicio->posicao);
+		
+		aux = pbuffer + sizeof(variaveis) + (sizeof(pessoa) * inicio->posicao) + sizeof(pessoa);
+		
+		for (inicio->i = 0; inicio->i < (inicio->contador - inicio->posicao); inicio->i++) {
+			strcpy(p->nome, aux->nome);
+			strcpy(p->cpf, aux->cpf);
+			p++;
+			aux++;
+		}	
+		if ((pbuffer = (pessoa *) realloc(pbuffer, sizeof(variaveis) + (sizeof(pessoa) * inicio->contador))) != NULL) {
+			printf("\n-------------------------\nPessoa excluida com sucesso...\n-------------------------\n");	
+			inicio->contador--;
+		}	
+	}		
+	
+	
+}	
+void buscar() {
+	
+	
+	printf("Nome a ser buscado: ");
+	scanf("%s", inicio->nome);
+	
+	pessoa *p;
+	
+	inicio->achei = 0;
+	
+	p = pbuffer + sizeof(variaveis);
+	
+	for (inicio->i = 0; inicio->i < inicio->contador; inicio->i++) {
+		if (strcmp(p->nome, inicio->nome) == 0) {
+			inicio->achei = 1;	
+			printf("\n------------------------\n");
+			printf("Nome: %s\nCPF: %s\n", p->nome, p->cpf);
+			printf("------------------------\n");
+		}	
+		p++;
+	}	
+	
+	if (inicio->achei == 0) {
+		printf("\n------------------------\n");
+		printf("  Nome nao encontrado...\n");
+		printf("------------------------\n");	
+	}	
+	
 	
 }	
